b9ac7675c87a301c426b6e78cd9492ae7899069c
danielaniceto <danielgustavots@hotmail.com>
2024-01-11 10:21:53 -0300

    This is a final solution the exercise five

--- exercicio_cinco.py
+++ exercicio_cinco.py
@@ -2,34 +2,36 @@
 
 def is_validation_number(first_number, second_number, third_number):
     if not isinstance(first_number, (float, int)):
-        raise Exception("Please, enter a valid number!!!")
+        raise Exception("Please, enter a valid number!!!\n")
     
     elif not isinstance(second_number,(float, int)):
-        raise Exception("Please, enter a valid number!!!")
+        raise Exception("Please, enter a valid number!!!\n")
     
     elif not isinstance(third_number,(float, int)):
-        raise Exception("Please, enter a valid number!!!")
+        raise Exception("Please, enter a valid number!!!\n")
     
     else:
-        return "It's OK."
+        return "It's OK.\n"
     
 def is_processing_higher_number(first_number, second_number, third_number):
     if first_number > second_number and first_number > third_number:
-        return f"The largest number entered was the {first_number}"
+        return f"The largest number entered was the {first_number}\n"
     
     elif second_number > first_number and second_number > third_number:
-        return f"The largest number entered was the {second_number}"
+        return f"The largest number entered was the {second_number}\n"
     
     else:
-        return f"The largest number entered was the{third_number}"
+        return f"The largest number entered was the {third_number}\n"
 
 def main():
-    first_number = float(input("Enter the first number here: "))
-    second_number = float(input("Enter the second number here: "))
-    third_number = float(input("Enter the third number here: "))
+    first_number = float(input("Enter the first number here:"))
+    second_number = float(input("Enter the second number here:"))
+    third_number = float(input("Enter the third number here:"))
 
     validation_numbers = is_validation_number(first_number, second_number, third_number)
     print(validation_numbers)
 
     higher_number = is_processing_higher_number(first_number, second_number, third_number)
     print(higher_number)
+
+main()
