c4e2d3c4d51caa76a5f36962acce80b933339734
danielaniceto <danielgustavots@hotmail.com>
2024-01-11 10:11:51 -0300

    The parcial solution the five exercise

--- exercicio_cinco.py
+++ exercicio_cinco.py
@@ -1,1 +1,35 @@
 #Esse programa terá por função, receber do usuário 3 numeros, processar e apresentar o maior deste 3 numeros digitados
+
+def is_validation_number(first_number, second_number, third_number):
+    if not isinstance(first_number, (float, int)):
+        raise Exception("Please, enter a valid number!!!")
+    
+    elif not isinstance(second_number,(float, int)):
+        raise Exception("Please, enter a valid number!!!")
+    
+    elif not isinstance(third_number,(float, int)):
+        raise Exception("Please, enter a valid number!!!")
+    
+    else:
+        return "It's OK."
+    
+def is_processing_higher_number(first_number, second_number, third_number):
+    if first_number > second_number and first_number > third_number:
+        return f"The largest number entered was the {first_number}"
+    
+    elif second_number > first_number and second_number > third_number:
+        return f"The largest number entered was the {second_number}"
+    
+    else:
+        return f"The largest number entered was the{third_number}"
+
+def main():
+    first_number = float(input("Enter the first number here: "))
+    second_number = float(input("Enter the second number here: "))
+    third_number = float(input("Enter the third number here: "))
+
+    validation_numbers = is_validation_number(first_number, second_number, third_number)
+    print(validation_numbers)
+
+    higher_number = is_processing_higher_number(first_number, second_number, third_number)
+    print(higher_number)
