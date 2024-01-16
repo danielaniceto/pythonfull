8b97857c251db1783c718c73055e0109ae8c3deb
danielaniceto <danielgustavots@hotmail.com>
2024-01-16 11:04:30 -0300

    The final resolution exercise ten

--- exercicio_dez.py
+++ exercicio_dez.py
@@ -1,20 +1,24 @@
 """O foco desse programa e receber uma nota de um aluno de 0 a 10, e validar se o valor digitado está dentro desse range de valores aceitos"""
 
+import sys
+
 def is_validation_type_grade(student_grade):
     if not isinstance(student_grade, (float, int)):
         raise Exception ("The format this grade is not valid!!! \n")
     
     elif student_grade < 0:
-        raise Exception ("Its no possible to have grades below zero")
+        raise Exception ("Its no possible to have grades below zero\n")
     
     else:
-        return "The grades type is correct!!!"
+        return "The grades type is correct!!!\n"
     
 def is_analys_range_grade(student_grade):
-    ranges_grades = range(0, 10, 1)
-    for range_grade in ranges_grades:
-        if student_grade < range_grade.start and student_grade < range_grade.stop:
-            return f"{student_grade}, is outside the accepted grade range"
+    ranges_grades = range(0, 10)
+    if student_grade in ranges_grades:
+        return f"{student_grade}, is outside the accepted grade range"
+    
+    else:
+        return f"{student_grade}, is not outsite the accepted grade range"
 
 def main():
     student_grade = int(input("Please enter a grade this your student for validation: "))
