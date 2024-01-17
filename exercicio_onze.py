b37981c409d29a8f41611ee561c5412446cb181e
danielaniceto <danielgustavots@hotmail.com>
2024-01-17 15:28:04 -0300

    The final solution, exercise eleven

--- exercicio_onze.py
+++ exercicio_onze.py
@@ -1,20 +1,45 @@
 """Defina um usuário e senha, e faça a validação deste,
 recenbendo do user seus acesso e depois validando"""
 
-def is_validation_user():
+def is_validation_user(user_validation, USER):
+    if user_validation == USER:
+        return True
+    else:
+        return "\n Sorry, user or password is not ok, please check your acess \n"
 
+def ins_validation_password(password_validation, PASSWORD):
+    if password_validation == PASSWORD:
+        return True
+    
+    else:
+        return "Sorry, user or password is not ok, please check your acess \n"
+
+def is_validation_acess_user(password_validation_system, user_validation_system):
+    if password_validation_system == True and user_validation_system == True:
+        return "Acess ok!!!"
+    
+    else:
+        return "Acess blocked"
 
 def main():
-    user = "Daniel"
-    password = "1234abcd"
+    while True:
+        USER = "Daniel"
+        PASSWORD = "1234abcd"
 
-    user_validation = input("Please, enter your user for login in this program: ")
-    password_validation = input("Please, enter your passowrd now: ")
+        user_validation = input("Please, enter your user for login in this program: ")
+        password_validation = input("Please, enter your passowrd now: ")
 
-    user_validation_system = is_validation_user(user_validation, user)
-    print(user_validation_system)
+        user_validation_system = is_validation_user(user_validation, USER)
+        print(user_validation_system)
 
-    password_validation_system = ins_validation_password(password_validation, password)
-    print(password_validation_system)
+        password_validation_system = ins_validation_password(password_validation, PASSWORD)
+        print(password_validation_system)
 
-    if user_validation_system == True and password_validation_system == True:
+        validation_acess_user = is_validation_acess_user(user_validation_system, password_validation_system)
+        print(validation_acess_user)
+        
+        if validation_acess_user == "Acess ok!!!":
+            break
+
+if __name__ == "__main__":
+    main()
