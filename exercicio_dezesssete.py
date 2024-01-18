820d255f830ee496e8c29a5afe4f927167e08109
danielaniceto <danielgustavots@hotmail.com>
2024-01-18 16:59:23 -0300

    The final solution exercise seveteen

--- exercicio_dezesssete.py
+++ exercicio_dezesssete.py
@@ -1,3 +1,11 @@
 """O foco deste programa e usar o laço FOR para montar uma tabuada completa
 sem influência do usuário"""
 
+def main():
+    for i in range(0, 11):
+        for j in range(0, 11):
+            print(f"{i} x {j} = {i * j}")
+        i += 1
+        j += 1
+if __name__ == "__main__":
+    main()
