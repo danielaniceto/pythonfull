8d3c1596c3248984a809349e8d0d7d78d94db3c8
danielaniceto <danielgustavots@hotmail.com>
2024-01-19 09:44:01 -0300

    Parcial solution

--- exercicio_dezoito.py
+++ exercicio_dezoito.py
@@ -1,3 +1,10 @@
 """O objetivo deste programa e apresentar apenas numeros pares, em um range de
 numeros, começando por 0 a 1000, usando o laço de repetição FOR"""
 
+def main():
+    i = 0
+    for i in range(0, 1000):
+            print()
+        i += 1
+if __name__ == "__main__":
+    main()
