298513c5474aa2ddcfd8e4734a9e1b63d3c9ddcd
danielaniceto <danielgustavots@hotmail.com>
2024-01-23 10:20:07 -0300

    Evolution nineteen exercise

--- exercicio_dezenove.py
+++ exercicio_dezenove.py
@@ -1,1 +1,26 @@
-#Receber 10 valores, e mostrar a soma de todos eles, isso usando listas
+#Receber 10 valores, e mostrar a soma de todos eles, isso usando listas, a ideia e sempre separar todas as partes do codigo em funções diferentes
+
+def is_sum_numbers_list(list_numbers):
+    cont = 0
+    for cont in range(0, 10):
+        numbers_sum = list_numbers[cont]
+
+
+def main():
+    list_numbers = []
+    cont = 0
+
+    for cont in range (0, 10):
+        numbers = int(input("Take the number here: "))
+        list_numbers.append(numbers)
+        cont +=1
+
+    numbers_sum = is_sum_numbers_list(list_numbers)
+    print(numbers_sum)
+
+if __name__ == "__main__":
+    main()
+
+
+        
+
