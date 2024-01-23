28398913bcb6fc896cc7eae30efd35cefb491a1e
danielaniceto <danielgustavots@hotmail.com>
2024-01-23 14:04:21 -0300

    The final solution exercise nineteen

--- exercicio_dezenove.py
+++ exercicio_dezenove.py
@@ -1,11 +1,12 @@
-#Receber 10 valores, e mostrar a soma de todos eles, isso usando listas, a ideia e sempre separar todas as partes do codigo em funções diferentes
+"""Receber 10 valores, e mostrar a soma de todos eles, isso usando listas, a ideia e
+sempre separar todas as partes do codigo em funções diferentes"""
 
 def is_sum_numbers_list(list_numbers):
     
     return sum(list_numbers)
 
-def is_Verification_list(list_numbers):
-    if type(list_numbers) != int:
+def is_verification_list(list_numbers):
+    if isinstance(list_numbers, (float, int)):
         return False
         
     else:
@@ -16,11 +17,13 @@ def main():
     cont = 0
 
     for cont in range (0, 10):
-        numbers = int(input("Take the number here: "))
+        numbers = float(input("Insert the number here: "))
         list_numbers.append(numbers)
         cont +=1
 
-    is_Verification_list(list_numbers)
+    verification_list = is_verification_list(list_numbers)
+    if verification_list == False:
+        print("there is something wrong with your numbers, please review")
 
     numbers_sum = is_sum_numbers_list(list_numbers)
     print(numbers_sum)
