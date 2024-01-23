d4e3ab0377e929e0266967d07f45e4ea126cf82b
danielaniceto <danielgustavots@hotmail.com>
2024-01-23 13:08:02 -0300

    Parcial solution exercise nineteen

--- exercicio_dezenove.py
+++ exercicio_dezenove.py
@@ -1,10 +1,15 @@
 #Receber 10 valores, e mostrar a soma de todos eles, isso usando listas, a ideia e sempre separar todas as partes do codigo em funções diferentes
 
 def is_sum_numbers_list(list_numbers):
-    cont = 0
-    for cont in range(0, 10):
-        numbers_sum = list_numbers[cont]
+    
+    return sum(list_numbers)
 
+def is_Verification_list(list_numbers):
+    if type(list_numbers) != int:
+        return False
+        
+    else:
+        return True   
 
 def main():
     list_numbers = []
@@ -15,12 +20,10 @@ def main():
         list_numbers.append(numbers)
         cont +=1
 
+    is_Verification_list(list_numbers)
+
     numbers_sum = is_sum_numbers_list(list_numbers)
     print(numbers_sum)
 
 if __name__ == "__main__":
     main()
-
-
-        
-
