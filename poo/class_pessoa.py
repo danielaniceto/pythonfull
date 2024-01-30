a908ff6b0027e103e537a8175410e213cf1e1fcf
danielaniceto <danielgustavots@hotmail.com>
2024-01-30 10:29:49 -0300

    Inicio dos estudos sobre POO

---
+++ poo/class_pessoa.py
@@ -0,0 +1,10 @@
+"""O objetivo desse programa e trabalhar e aprimorar a construção e o uso de funções e classes"""
+
+class PessoasClientes():
+    def __init__(self, nome, idade, cpf, endereco):
+        self.nome = nome
+        self.idade = idade
+        self.cpf = cpf
+        self.endereco = endereco
+
+primeiro_cliente = PessoasClientes()
