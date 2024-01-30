157819187c7afc0861799e8bcae8d074e23d4fa9
danielaniceto <danielgustavots@hotmail.com>
2024-01-30 12:39:33 -0300

    Melhorias de codigo, import de bibliotecas, envio de dados do cliente da coleta para a classe

--- poo/colhe_infos_clientes.py
+++ poo/colhe_infos_clientes.py
@@ -1,5 +1,7 @@
 """O objetivo desse programa e trabalhar e aprimorar a construção e o uso de funções e classes"""
 
+from class_pessoa import PessoasClientes
+
 def main():
 
     nome_cliente = input("Digite seu nome para cadastro: ")
@@ -7,6 +9,8 @@ def main():
     cpf_cliente = int(input("Digite seu CPF aqui: "))
     endereco_cliente = input("Digite seu endereço aqui: ")
 
+    PessoasClientes(nome_cliente, idade_cliente, cpf_cliente, endereco_cliente)
+
 if __name__ == "__main__":
     main()
-    
+
