0ca9772fa04e39b0960cc4050f14cb59f6697f39
danielaniceto <danielgustavots@hotmail.com>
2024-01-30 11:24:26 -0300

    Evolução classe e POO

---
+++ poo/colhe_infos_clientes.py
@@ -0,0 +1,11 @@
+"""O objetivo desse programa e trabalhar e aprimorar a construção e o uso de funções e classes"""
+
+def main():
+
+    nome_cliente = input("Digite seu nome para cadastro: ")
+    idade_cliente = int(input("Digite sua idade: "))
+    cpf_cliente = int(input("Digite seu CPF aqui: "))
+    endereco_cliente = input("Digite seu endereço aqui: ")
+
+if __name__ == "__main__":
+    main()
