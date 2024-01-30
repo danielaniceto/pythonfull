a1e430079079a4df070d7dfef3e0dcbb84e92916
danielaniceto <danielgustavots@hotmail.com>
2024-01-30 12:51:44 -0300

    Teste de codigos

--- poo/mostra_cliente.py
+++ poo/mostra_cliente.py
@@ -2,7 +2,7 @@
 
 from class_pessoa import PessoasClientes
 
-def forma_mensagem_cliente():
+def forma_mensagem_cliente(self):
 
     infos_pessoa = (f"Olá {PessoasClientes.nome}, seu cadastro foi feito com sucesso, sua idade e de {PessoasClientes.idade},
         seu CPF e {PessoasClientes.cpf}, e seu endereço e {PessoasClientes.endereco}")
