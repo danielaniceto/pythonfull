b4044985b1bb22b89245febdee0365795c2d07b9
danielaniceto <danielgustavots@hotmail.com>
2024-02-01 16:24:19 -0300

    Adequação de codigo

--- mvc/dal.py
+++ mvc/dal.py
@@ -5,12 +5,17 @@ class PessoaDal:
     def salvar(cls, pessoa: Pessoa):
         with open("pessoas.txt", "w") as arq:
             arq.write(pessoa.nome + " " + str(pessoa.idade) + " " + pessoa.cpf)
-
-    def ler(self):
+            
+    @classmethod
+    def ler(cls):
         nome = "Daniel"
         idade = 32
-        cpf: 123445667755
+        cpf = 12123566565
         return Pessoa(nome, idade, cpf)
 
 p1 = Pessoa("Daniel", 32, "12102910291")
 PessoaDal.salvar(p1)
+
+print(PessoaDal.ler().nome)
+print(PessoaDal.ler().idade)
+print(PessoaDal.ler().cpf)
